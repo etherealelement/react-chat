626b9a6b695137cf6a0d64742bc4512e97cfe312
etherealelement <vitalik.safonov@list.ru>
2024-10-24 06:25:22 +0300

    init

---
+++ src/app/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { queryClient } from "@/shared/api/query-client.ts";
+import { store } from "@/shared/redux.ts";
+
+import "./index.css";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App></App>
+      </Provider>
+      <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
+    </QueryClientProvider>
+    <App />
+  </StrictMode>,
+);
---
+++ src/shared/redux.ts
@@ -0,0 +1,27 @@
+import {
+  combineSlices,
+  createSelector,
+  ThunkAction,
+  UnknownAction,
+} from "@reduxjs/toolkit";
+import { useDispatch, useSelector, useStore } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+export const rootReducer = combineSlices();
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type AppState = any;
+export type AppDispatch = typeof store.dispatch;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type AppThunk<R = void> = ThunkAction<R, AppState, any, UnknownAction>;
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware();
+  },
+});
+
+export const useAppSelector = useSelector.withTypes<AppState>();
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppStore = () => useStore<typeof store>();
+export const createAppSelector = createSelector.withTypes<AppState>();
